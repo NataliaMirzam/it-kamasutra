87be024ca9b90c865424733d93cdb963d4376704
NataliaMirzam <natalia.alex.st@gmail.com>
2020-02-13 20:28:06 +0300

    Add lessons 1-13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src="https://lh3.googleusercontent.com/proxy/Xqh5e5w8y8W7UZKtEA-r7yQCjxKFtiVybVJK0UOU-BR15X7EpgEgOcLqBAbM3AC0_wOM1QQhrWZMT0YC4apRMLUo49EFBmejZg4c-jyAIF4oKRZFicHLmf-NwIKWsiHBk0e_9kSaw9lk1-2x" />
+  </header>
+}
+
+export default Header;
